109c2553f4f7936c7cefda0a5aa915037be1a9b9
Bhagawat Chapagain <bchapaga@stevens.edu>
2024-11-15 21:36:22 -0500

    final

--- checkhashmatches.py
+++ checkhashmatches.py
@@ -1,22 +1,31 @@
+import os
+
 def checkMatches(file):
     f = open(file, 'r')
-    lines = [line.strip().replace(" ", "") for line in f.readlines()]
-
-    leaked = open("leakedhashes.txt", "r")
-    leaked_lines = leaked.readlines()                                                                                                                                                                                                                                                                                                                                       
-
-    leaked_lines = [line.strip().replace(" ", "").split(":") for line in leaked_lines]
-    leaked_dict = {key: value for key, value in leaked_lines}
+    passwords = [line.strip().replace(" ", "").split(":") for line in f.readlines()]
+    pw_hashes = {key: value for key, value in passwords}
     
-    #print(lines)
-    #print(leaked_dict.values())
+    #print(pw_hsh)
 
-    for value in lines:
-        if value in leaked_dict.values(): print(            )
-        
-    # print(matching_values)
+    # Need to look at leaked hashes
+    # See if pw_hsh value matches leaked hashes value
+    
+    leaked = open("leakedhashes.txt", "r")                                                                                                                                                                                                                                                                                                         
+    leaked_lines = [line.strip().replace(" ", "").split(":") for line in leaked.readlines()]
+    user_pw_leaked_hash = {key: value for key, value in leaked_lines}
 
-    f.close()
-    leaked.close()
+    hits = open("hits.txt", "w")
 
-checkMatches("rockyou2000sha256_dummy.txt")
+    for pw_hsh in pw_hashes.values():
+        if(pw_hsh in user_pw_leaked_hash.values()):
+            hits.write(f"{list(user_pw_leaked_hash.keys())[list(user_pw_leaked_hash.values()).index(pw_hsh)]}:{list(pw_hashes.keys())[list(pw_hashes.values()).index(pw_hsh)]}")
+            hits.write("\n")
+    hits.close()
+
+
+print("SHA256 Matches:")
+checkMatches("rockyou2000sha256.txt")
+
+print("MD5 Matches:")
+checkMatches("rockyou2000md5.txt")
+
